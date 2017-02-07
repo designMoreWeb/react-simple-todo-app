ee58f1c4a268f8c413134a457245faa4f1b517ae
designMoreWeb <divystolia@gmail.com>
2017-02-07 18:58:58 -0500

    Added an ES linter file working on refactoring React Components to be Stateless Functional Components

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {TodoForm} from './components/todo/TodoForm'
+import {TodoForm, TodoList} from './components/todo'
 
 class App extends Component {
   constructor(){
@@ -30,7 +30,8 @@ class App extends Component {
         </div>
           <div className="Todo-App">
             <TodoForm handleInputChange={this.handleInputChange}
-            currentTodo={this.state.currentTodo}/>
+              currentTodo={this.state.currentTodo}/>
+            <TodoList/>
             <div className="Todo-List">
               <ul>
                 {this.state.todos.map(todo =>
---
+++ react-app/src/components/todo/TodoList.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const TodoList = (props) =>{
+  return(
+    <div>ToDoList</div>
+  )
+}
